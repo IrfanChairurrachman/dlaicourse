64439cc21c9501b019f1609ed6ece96d2a37eccc
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2021-05-22 14:12:39 +0700

    feat: done c1w4

--- TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 4/Exercise/index_exercise.js
+++ TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 4/Exercise/index_exercise.js
@@ -27,18 +27,20 @@ async function train() {
     layers: [
         
       // YOUR CODE HERE
-
+      tf.layers.flatten({inputShape: mobilenet.outputs[0].shape.slice(1)}),
+      tf.layers.dense({ units: 100, activation: 'relu'}),
+      tf.layers.dense({ units: 5, activation: 'softmax'})
     ]
   });
     
    
   // Set the optimizer to be tf.train.adam() with a learning rate of 0.0001.
-  const optimizer = // YOUR CODE HERE
+  const optimizer = tf.train.adam(0.0001);
     
         
   // Compile the model using the categoricalCrossentropy loss, and
   // the optimizer you defined above.
-  model.compile(// YOUR CODE HERE);
+  model.compile({optimizer: optimizer, loss: 'categoricalCrossentropy'});
  
   let loss = 0;
   model.fit(dataset.xs, dataset.ys, {
@@ -108,11 +110,14 @@ async function predict() {
 		case 3:
 			predictionText = "I see Spock";
 			break;
-            
-        // Add a case for lizard samples.
-        // HINT: Look at the previous cases.
-            
-        // YOUR CODE HERE 
+      
+      // Add a case for lizard samples.
+      // HINT: Look at the previous cases.
+      
+      // YOUR CODE HERE 
+    case 4:
+      predictionText = "I see Lizard";
+      break;
 	
             
 	}
